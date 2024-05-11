cda291ca92406bfb68d0de86d30131907e797f6c
nsirtak <nsirtak@gmail.com>
2024-05-10 23:56:06 -0500

    part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,64 +1,64 @@
-const input = require('readline-sync');
+  const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
+  // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
-let candidateName = "";
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer= "Sally Ride";
-let candidateAnswer = "";
-
-
-//TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+  // TODO 1.1a: Define candidateName // 
+  let candidateName = "";
+  // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+  let question = "Who was the first American woman in space? ";
+  let correctAnswer= "Sally Ride";
+  let candidateAnswer = "";
 
 
-function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name?");
-}
+  //TODO: Variables for Part 2
+  let questions;
+  let correctAnswers;
+  let candidateAnswers;
 
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
-}
-function gradeQuiz(candidateAnswers) {
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  
-  console.log('Your answer:', candidateAnswer);
-  console.log('Correct answer:', correctAnswer);
-  const isCorrect = candidateAnswer.toLowerCase() === correctAnswer.toLowerCase();
-  if (isCorrect) {
-    console.log("Correct answer!");
-  } else {
-    console.log("Incorrect answer. Try again later");
+
+  function askForName() {
+    // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("What is your name?");
   }
 
-  let grade; //TODO 3.2 use this variable to calculate the candidates score.
+  function askQuestion() {
+    // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+    candidateAnswer = input.question(question);
+  }
+  function gradeQuiz(candidateAnswers) {
+    // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+    
+    console.log('Your answer:', candidateAnswer);
+    console.log('Correct answer:', correctAnswer);
+    const isCorrect = candidateAnswer.toLowerCase() === correctAnswer.toLowerCase();
+    if (isCorrect) {
+      console.log("Correct answer!");
+    } else {
+      console.log("Incorrect answer. Try again later");
+    }
 
-  return grade;
+    let grade; //TODO 3.2 use this variable to calculate the candidates score.
 
-}
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Greet candidate using their name //
-  console.log(`Greetings, ${candidateName}` );
-  askQuestion();
-  gradeQuiz(this.candidateAnswer);
-}
+    return grade;
 
-// ----------- Don't write any code or change any code below this line ---------- //
-module.exports = {
-  candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
-  questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
-  runProgram: runProgram
-};
+  }
+  function runProgram() {
+    askForName();
+    // TODO 1.1c: Greet candidate using their name //
+    console.log(`Greetings, ${candidateName}` );
+    askQuestion();
+    gradeQuiz(this.candidateAnswer);
+  }
+
+  // ----------- Don't write any code or change any code below this line ---------- //
+  module.exports = {
+    candidateName: candidateName,
+    question: question,
+    correctAnswer: correctAnswer,
+    candidateAnswer: candidateAnswer,
+    questions: questions,
+    correctAnswers: correctAnswers,
+    candidateAnswers: candidateAnswers,
+    gradeQuiz: gradeQuiz,
+    runProgram: runProgram
+  };
