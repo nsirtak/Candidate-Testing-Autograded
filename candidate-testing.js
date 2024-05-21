e5408a97f0500220374758c04b6f323a4fbfc4ea
nsirtak <nsirtak@gmail.com>
2024-05-21 06:35:20 -0500

    part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -31,7 +31,6 @@ let candidateAnswers=[];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("What is your name?");
-console.log("CandidateName:" +candidateName);
 }
 
 function askQuestion() {
